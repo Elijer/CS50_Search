5c04a5355a731184670ec2179d24cdd9e2d6fb6d
Jah <kennedye2@vcu.edu>
2021-04-17 12:43:30 -0400

    image page now searches for images

---
+++ reverse_engineer.js
@@ -0,0 +1,24 @@
+/*normal search =>
+https://www.google.com/search?q=moose
+&sxsrf=ALeKk02sLRWzGkEUVw6WfP3Co3ylZQ_dzg%3A1618676892655
+&ei=nAx7YKK6J7DR5NoP29-H-AI
+&oq=moose
+&gs_lcp=Cgdnd3Mtd2l6EAMyBwgjELADECcyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyCgguELADEMgDEEMyCgguELADEMgDEEMyCgguELADEMgDEEMyDQguELADEMgDEEMQiwMyDQguELADEMgDEEMQiwNKBQg4EgExUABYAGCLIWgBcAJ4AIABY4gBY5IBATGYAQCqAQdnd3Mtd2l6yAEOuAEBwAEB
+&sclient=gws-wiz
+&ved=0ahUKEwii-MSp2YXwAhWwKFkFHdvvAS8Q4dUDCA8
+&uact=5*/
+
+/*
+img search =>
+https://www.google.com/search?q=moose
+&sxsrf=ALeKk02sLRWzGkEUVw6WfP3Co3ylZQ_dzg:1618676892655
+&source=lnms
+&tbm=isch
+&sa=X
+&ved=2ahUKEwii-MSp2YXwAhWwKFkFHdvvAS8Q_AUoAXoECAEQAw
+&cshid=1618676924887221
+&biw=1440
+&bih=789
+*/
+
+var imageSection = "&tbm=isch"
